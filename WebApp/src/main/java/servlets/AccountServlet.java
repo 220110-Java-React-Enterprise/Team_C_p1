37d880f1da6e7c826bf921743e04c7f6538cd1d4
Donald Farrar <donfar09@gmail.com>
2022-02-08 13:02:14 -0800

    all exceptions are handled

--- WebApp/src/main/java/servlets/AccountServlet.java
+++ WebApp/src/main/java/servlets/AccountServlet.java
@@ -33,32 +33,56 @@ public class AccountServlet extends HttpServlet {
             FileLogger.getFileLogger().log(e);
             resp.setStatus(500);
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws CustomException {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            AccountObject payload = mapper.readValue(req.getInputStream(), AccountObject.class);
+            AccountStore.setAccountObj(payload);
+            resp.setStatus(203);
+            resp.getWriter().print("Account successfully created.");
+        } catch (JsonProcessingException e) {
+            throw new CustomException("This account does not seem to exist!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            FileLogger.getFileLogger().log(e);
+            resp.setStatus(500);
+        }
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        AccountObject payload = mapper.readValue(req.getInputStream(), AccountObject.class);
-        AccountStore.setAccountObj(payload);
-        resp.setStatus(203);
-        resp.getWriter().print("Account successfully created.");
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws CustomException {
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            AccountObject payload = mapper.readValue(req.getInputStream(), AccountObject.class);
+            //AccountStore.updateAccount(payload);
+            AccountStore.setAccountObj(payload);
+            resp.setStatus(203);
+            resp.getWriter().print("There has been a change to the accounts information.");
+        } catch (JsonProcessingException e) {
+            throw new CustomException("This change cannot be made.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            FileLogger.getFileLogger().log(e);
+            resp.setStatus(500);
+        }
     }
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        AccountObject payload = mapper.readValue(req.getInputStream(), AccountObject.class);
-        //AccountStore.updateAccount(payload);
-        AccountStore.setAccountObj(payload);
-        resp.setStatus(203);
-        resp.getWriter().print("There has been a change to the accounts information.");
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        AccountStore.setAccountObj(null);
-        resp.setStatus(203);
-        resp.getWriter().print("Account has been deleted.");
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws CustomException {
+        try{
+            AccountStore.setAccountObj(null);
+            resp.setStatus(203);
+            resp.getWriter().print("Account has been deleted.");
+        } catch (JsonProcessingException e) {
+            throw new CustomException("This request cannot be made.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            FileLogger.getFileLogger().log(e);
+            resp.setStatus(500);
+        }
     }
 }
--- WebApp/src/main/java/servlets/CommentServlet.java
+++ WebApp/src/main/java/servlets/CommentServlet.java
@@ -1,9 +1,12 @@
 package servlets;
 
 import User.UserObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import Comment.CommentObject;
 import Comment.CommentStore;
+import exceptions.CustomException;
+import utils.FileLogger;
 
 
 import javax.servlet.ServletException;
@@ -14,37 +17,69 @@ import java.io.IOException;
 
 public class CommentServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        CommentObject commentObj = CommentStore.getCommentObj();
-        ObjectMapper mapper = new ObjectMapper();
-        String Json = mapper.writeValueAsString(commentObj);
-        resp.getWriter().print(Json);
-        resp.setStatus(200);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws CustomException {
+        try{
+            CommentObject commentObj = CommentStore.getCommentObj();
+            ObjectMapper mapper = new ObjectMapper();
+            String Json = mapper.writeValueAsString(commentObj);
+            resp.getWriter().print(Json);
+            resp.setStatus(200);
+        } catch (JsonProcessingException e) {
+            throw new CustomException("This comment does not seem to exist!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            FileLogger.getFileLogger().log(e);
+            resp.setStatus(500);
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        CommentObject payload = mapper.readValue(req.getInputStream(), CommentObject.class);
-        CommentStore.setCommentObj(payload);
-        resp.setStatus(203);
-        resp.getWriter().print("New comment added to post.");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws CustomException {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            CommentObject payload = mapper.readValue(req.getInputStream(), CommentObject.class);
+            CommentStore.setCommentObj(payload);
+            resp.setStatus(203);
+            resp.getWriter().print("New comment added to post.");
+        } catch (JsonProcessingException e) {
+            throw new CustomException("Unable to process this request.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            FileLogger.getFileLogger().log(e);
+            resp.setStatus(500);
+        }
     }
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        CommentObject payload = mapper.readValue(req.getInputStream(), CommentObject.class);
-        CommentStore.setCommentObj(payload);
-        //CommentStore.updateComment(payload);
-        resp.setStatus(203);
-        resp.getWriter().print("There has been a change to the comment.");
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws CustomException {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            CommentObject payload = mapper.readValue(req.getInputStream(), CommentObject.class);
+            CommentStore.setCommentObj(payload);
+            //CommentStore.updateComment(payload);
+            resp.setStatus(203);
+            resp.getWriter().print("There has been a change to the comment.");
+        } catch (JsonProcessingException e) {
+            throw new CustomException("This change cannot be made.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            FileLogger.getFileLogger().log(e);
+            resp.setStatus(500);
+        }
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        CommentStore.setCommentObj(null);
-        resp.setStatus(203);
-        resp.getWriter().print("Comment has been deleted.");
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws CustomException {
+        try {
+            CommentStore.setCommentObj(null);
+            resp.setStatus(203);
+            resp.getWriter().print("Comment has been deleted.");
+        } catch (JsonProcessingException e) {
+            throw new CustomException("This request cannot be made.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            FileLogger.getFileLogger().log(e);
+            resp.setStatus(500);
+        }
     }
 }
